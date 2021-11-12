66187ffbd504dca793ecbd25d48f1a0f5520acdc
RiteshGelada <94109372+RiteshGelada@users.noreply.github.com>
2021-11-12 13:44:10 +0530

    Add files via upload

---
+++ data type 2.py
@@ -0,0 +1,19 @@
+x = ("Ritesh", "Sanat", "Sanket", "Dikshant")
+y = {"Ritesh", "Sanat", "Sanket", "Dikshant"}
+z = True
+q = range(11)
+w = {"Name" : "Riteh", "Age" : 22}
+e = frozenset({"Ritesh", "Sanat", "Sanket", "Dikshant"})
+r = b"Always Here"
+t = bytearray(45)
+u = memoryview(bytes(45))
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(q))
+print(type(w))
+print(type(e))
+print(type(r))
+print(type(t))
+print(type(u))
+
