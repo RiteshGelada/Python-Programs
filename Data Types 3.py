e0a513cca1900a8ce252cc4d5c2898cf2adeb8b7
RiteshGelada <94109372+RiteshGelada@users.noreply.github.com>
2021-11-14 13:09:10 +0530

    Add files via upload

---
+++ Data Types 3.py
@@ -0,0 +1,16 @@
+x = str("Hii Bro")
+y = float(45.1)
+z = int(45)
+q = complex(4j)
+w = range(7)
+e = tuple(("Fruits", "Veggies", "Dry fruits", "Nuts"))
+t = dict(Name="Ritesh", Age=23)
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(q))
+print(type(w))
+print(type(e))
+print(type(t))
+
+
