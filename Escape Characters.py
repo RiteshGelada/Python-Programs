32f055c92f4bfa2c099f42102ded5e77ed0a0ec8
RiteshGelada <94109372+RiteshGelada@users.noreply.github.com>
2021-11-23 12:56:51 +0530

    Add files via upload

---
+++ Escape Characters.py
@@ -0,0 +1,12 @@
+txt = "We are the so-called \"Vikings\" from the north."
+print(txt)
+txt = 'That\'s ok.'
+print(txt)
+txt = "Hello\nWorld!"
+print(txt)
+txt = "Hello\tWorld"
+print(txt)
+txt = "Hello \bWorld!"
+print(txt)
+txt = "\110\145\154\154\155"
+print(txt)
