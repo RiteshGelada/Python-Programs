aa18cb1303fe922a7356c8162eed9300ecd6922f
RiteshGelada <94109372+RiteshGelada@users.noreply.github.com>
2021-11-24 09:50:34 +0530

    Add files via upload

---
+++ Python booliens 2.py
@@ -0,0 +1,23 @@
+print(bool("Hey!"))
+print(bool(123))
+print(bool(["Ritesh", "Sanat", "Ritesh"]))
+print(bool("abc"))
+print(bool(False))
+print(bool(()))
+print(bool({}))
+print(bool(([])))
+print(bool(""))
+
+class myclass():
+  def __len__(self):
+    return 0
+
+myobj = myclass()
+print(bool(myobj))
+
+class myclass():
+   def __len__(selfself):
+      return 0
+
+myobj = myclass()
+print(bool(myobj))
